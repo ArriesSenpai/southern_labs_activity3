596102f64e1549a251fc58859c1e02c6676e4e92
ArriesSenpai <benneelemart@gmail.com>
2026-08-19 16:01:46 +0200

    Added working student_info.py script

---
+++ student_info.py
@@ -0,0 +1,12 @@
+#Author: Lemart
+name = input("Enter name: ")
+student_id = input("Enter student ID: ")
+favorite = input("Enter favorite: ")
+
+print("\nYour name is:", name)
+print("Your ID is:", student_id)
+print("Your favorite is:", favorite)
+
+age = int(input("Enter your Age: "))
+print("Your age is:", age + 1)
+
